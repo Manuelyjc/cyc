db199afc78304640cc74c0b53202e0a8054868fd
Manuelyjc <114593318+Manuelyjc@users.noreply.github.com>
2025-06-09 19:45:43 -0500

    primera parte finalida

---
+++ assets/js/app.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+  // Validar si el dispositivo soporta hover
+  if (window.matchMedia('(hover: hover)').matches) {
+    $('body').addClass('hover-supported');
+  } else {
+    $('body').removeClass('hover-supported');
+  }
+  if ($('body').hasClass('hover-supported')) {
+    console.log('El dispositivo soporta hover.');
+  } else {
+    console.log('El dispositivo no soporta hover.');
+  }
+
+  // ABRIR / CERRAR MENU
+  $(document).on('click', '.menu-toggle', function () {
+    $("header").toggleClass("open");
+  });
+
+  // EFECTOS
+  $(document).ready(function () {
+    const $items = $('.content-grid-hen .item');
+
+    function revealRandomItem() {
+      const randomIndex = Math.floor(Math.random() * $items.length);
+      const $item = $items.eq(randomIndex);
+
+      // Evita activar repetidamente el mismo item si ya está activo
+      if (!$item.hasClass('reveal')) {
+        $item.addClass('reveal');
+
+        // Luego de 2 segundos, vuelve a poner el fondo
+        setTimeout(() => {
+          $item.removeClass('reveal');
+        }, 2000);
+      }
+    }
+
+    // Ejecuta una vez por segundo
+    setInterval(revealRandomItem, 1000);
+  });
+
+
+  // SLIDER
+  $('.fade').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    autoplay: true,
+    speed: 300,
+    fade: true,
+    cssEase: 'linear'
+  });
+
+  // SELECTIZE
+  $("select").selectize({
+  });
+});
