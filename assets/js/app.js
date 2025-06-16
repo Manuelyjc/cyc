919499b3c8622cd08c7a4abdb8c8db385f35c5ed
Manuelyjc <114593318+Manuelyjc@users.noreply.github.com>
2025-06-16 18:37:39 -0500

    proyectos inicio

--- assets/js/app.js
+++ assets/js/app.js
@@ -51,6 +51,22 @@ $(document).ready(function () {
     cssEase: 'linear'
   });
 
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+  });
+
   // SELECTIZE
   $("select").selectize({
   });
